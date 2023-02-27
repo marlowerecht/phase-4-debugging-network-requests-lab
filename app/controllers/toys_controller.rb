1c1ada605a0bd2698608b4843595ada382a9143d
Marlowe Recht <recht.marlowe@gmail.com>
2023-02-27 15:43:53 +0100

    commit tree 56d0e62aa7a049198d5e0325b47b9d73ad34c666

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,6 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def index
     toys = Toy.all
@@ -7,13 +8,14 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: :accepted
   end
 
   def destroy
@@ -26,6 +28,10 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
